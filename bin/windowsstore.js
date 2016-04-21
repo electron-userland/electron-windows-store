d4b193aa28102500dc246eba50bfd5cc45eba1ba
Felix Rieseberg <felix@felixrieseberg.com>
2016-04-20 22:07:37 -0700

    Update with flow

--- bin/windowsstore.js
+++ bin/windowsstore.js
@@ -4,10 +4,21 @@
  * Module dependencies.
  */
 
-const program = require('commander');
-const package = require('../package.json');
+const program    = require('commander');
+
+const package      = require('../package.json');
+const ensureParams = require('../lib/params');
+const zip          = require('../lib/zip');
+const flatten      = require('../lib/flatten');
 
 program
   .version(package.version)
-  .option('-i, --input-directory', 'Directory containing your application')
+  .option('-i, --input-directory <dir>', 'Directory containing your application')
+  .option('-o, --output-directory <dir>', 'Output directory for the appx')
+  .option('-f, --flatten', 'Flatten Node modules without warning')
   .parse(process.argv);
+
+ensureParams(program)
+    .then(() => flatten(program.inputDirectory, program.flatten))
+    .then(() => zip(program.inputDirectory, program.outputDirectory))
+    .catch(e => console.log(e));
---
+++ ps1/zip.ps1
@@ -0,0 +1,30 @@
+#
+# Takes a folder, turns it into a zip file.
+#
+
+[CmdletBinding(DefaultParameterSetName="Convert")]
+Param(
+    [Parameter(Mandatory=$True, ParameterSetName="Convert")]
+    [string]
+    [ValidateNotNullOrEmpty()]
+    $source,
+    
+    [Parameter(Mandatory=$True, ParameterSetName="Convert")]
+    [string]
+    [ValidateNotNullOrEmpty()]
+    $destination
+)
+
+If (-Not (Test-path $source)) {
+    return "Source directory cannot be found"
+}
+
+If (-Not (Test-path $destination)) {
+    return "Destination directory cannot be found"
+}
+
+# We're zipping using the sytem's compressor
+Add-Type -assembly "system.io.compression.filesystem"
+$zip = Join-Path -Path $destination -ChildPath windowsstore.zip
+
+[io.compression.zipfile]::CreateFromDirectory($source, $zip)
