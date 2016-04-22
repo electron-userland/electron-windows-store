d995c0c8526ac72ead2202552ffd2a5b97b61ed3
Rita Zhang <rita.z.zhang@gmail.com>
2016-04-21 19:32:39 -0700

    Automatically flattening packages

--- ps1/flattennpmmodules.ps1
+++ ps1/flattennpmmodules.ps1
@@ -1,13 +1,35 @@
-$dir = (get-item $PSScriptRoot).parent.FullName
+#
+# Takes a folder, flatten node modules within the folder.
+#
+
+[CmdletBinding(DefaultParameterSetName="Convert")]
+Param(
+    [Parameter(Mandatory=$True, ParameterSetName="Convert")]
+    [string]
+    [ValidateNotNullOrEmpty()]
+    $source
+)
+
+If (-Not (Test-path $source)) {
+    return "Source directory for flatten cannot be found"
+}
+
+$flattenbin = (get-item $PSScriptRoot).parent.FullName + '\node_modules\flatten-packages\bin\flatten'
+
+$dir = $source
 write-host "Flattening npm modules from this directory: " $dir
+
 if(!$dir.EndsWith('\')){
 	$dir = $dir + '\'
 }
+write-host "npm install..."
+cd $dir
+npm install
 write-host "Processing root level..."
 Get-ChildItem $dir -Directory | ForEach-Object {
 	if($_.Name -eq 'node_modules'){
 			write-host $dir
-			flatten-packages $dir
+			node $flattenbin $dir
 	}
 }
 
@@ -18,7 +40,7 @@ Get-ChildItem $dir -Directory | ForEach-Object {
 	Get-ChildItem $path -Directory | ForEach-Object{
 		if($_.Name -eq 'node_modules'){
 			write-host $path
-			flatten-packages $path
+			node $flattenbin $path
 		}
 	}
 	
@@ -34,7 +56,7 @@ Get-ChildItem $dir -Directory | ForEach-Object {
 		Get-ChildItem $subpath -Directory | ForEach-Object{
 			if($_.Name -eq 'node_modules'){
 				write-host $subpath
-				flatten-packages $subpath
+				node $flattenbin $subpath
 			}
 		}
 	}
