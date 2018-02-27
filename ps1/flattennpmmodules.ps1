a95f19f1d1b034cc075d63f50ea6f9c91a09b720
Machisté Quintana <machiste@slack-corp.com>
2018-02-26 18:05:16 -0800

    Use npm dedupe instead of flatten-packages

--- ps1/flattennpmmodules.ps1
+++ ps1/flattennpmmodules.ps1
@@ -14,21 +14,16 @@ If (-Not (Test-path $source)) {
     return "Source directory cannot be found"
 }
 
-$flattenbin = (get-item $PSScriptRoot).parent.FullName + '\node_modules\flatten-packages\bin\flatten'
-If (!(Test-Path $flattenbin)) {
-    $flattenbin = (get-item $PSScriptRoot).parent.FullName + '\..\flatten-packages\bin\flatten'
-}
-
 function flatMe([string]$path){
     if (!$path.EndsWith('\')) {
         $path = $path + '\'
     }
-    
+
     Write-Host "Getting subdirectories of: " $path
     Get-ChildItem $path -Directory | ForEach-Object {
         if($_.Name -eq 'node_modules'){
             Write-Host "Flattening: " $path
-            node $flattenbin $path
+            npm dedupe
         } else {
             flatMe($path + $_)
         }
