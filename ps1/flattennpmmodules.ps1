c3d67580387a32a00d985e48ff0de648d39b34b0
Felix Rieseberg <felix@felixrieseberg.com>
2018-03-01 19:52:59 +0200

    Revert "Merge pull request #96 from mnquintana/migrate-to-dedupe"
    This reverts commit f455743ec9bb709e82060af509d0de63bb7145eb, reversing
    changes made to d33be1108c77a48b22e0a6ee03085d1081a3131f.

--- ps1/flattennpmmodules.ps1
+++ ps1/flattennpmmodules.ps1
@@ -14,16 +14,21 @@ If (-Not (Test-path $source)) {
     return "Source directory cannot be found"
 }
 
+$flattenbin = (get-item $PSScriptRoot).parent.FullName + '\node_modules\flatten-packages\bin\flatten'
+If (!(Test-Path $flattenbin)) {
+    $flattenbin = (get-item $PSScriptRoot).parent.FullName + '\..\flatten-packages\bin\flatten'
+}
+
 function flatMe([string]$path){
     if (!$path.EndsWith('\')) {
         $path = $path + '\'
     }
-
+    
     Write-Host "Getting subdirectories of: " $path
     Get-ChildItem $path -Directory | ForEach-Object {
         if($_.Name -eq 'node_modules'){
             Write-Host "Flattening: " $path
-            npm dedupe
+            node $flattenbin $path
         } else {
             flatMe($path + $_)
         }
